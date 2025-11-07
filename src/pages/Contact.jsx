03fcf4b51976322cc18ea8414d0c0e5bf23ecd8c
Sasidhar <9908135565ks@gmail.com>
2025-11-07 21:44:51 +0530

    Imported library in contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from "framer-motion";
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
 
