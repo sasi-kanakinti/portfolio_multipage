0a57c36fcd8d5789865b8c6441fc6716c425e7b0
Sasidhar <9908135565ks@gmail.com>
2025-11-07 22:23:04 +0530

    Fixed 402 Error

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import App from "./App";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
